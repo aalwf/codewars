dfce7486e8b8ae73457919ba8c7e4896380ec202
Blurryface <2223594.elgin@smkn-2sbg.sch.id>
2024-05-20 10:41:38 +0700

    Sum Of Positive

--- sum-of-positive.js
+++ sum-of-positive.js
@@ -1,7 +1,6 @@
-// Bool To Word
+// Sum Of Positive
 // Day 6
 // https://www.codewars.com/kata/53369039d7ab3ac506000467
 
 const positiveSum = (...arr) =>
   arr.reduce((acc, curr) => (curr > 0 ? acc + curr : acc), 0);
-console.log(positiveSum(1, 2, 3, 4, 5));
