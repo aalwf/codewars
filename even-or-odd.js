764078bcdac876777a934a786112c1469875d2ee
Blurryface <2223594.elgin@smkn-2sbg.sch.id>
2024-05-20 09:45:28 +0700

    Return Negative

--- even-or-odd.js
+++ even-or-odd.js
@@ -1,5 +1,5 @@
 // Even Or Odd
-// Day 1
+// Day 2
 // https://www.codewars.com/kata/554e4a2f232cdd87d9000038
 
 const evenOrOdd = (number) => (number % 2 === 0 ? "Even" : "Odd");
---
+++ return-negative.js
@@ -0,0 +1,5 @@
+// Return Negative
+// Day 3
+// https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+
+const makeNegative = (number) => -Math.abs(number);
