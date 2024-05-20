8437aacd8d71949537421bb8362eeca5157eb625
Blurryface <2223594.elgin@smkn-2sbg.sch.id>
2024-05-20 10:44:07 +0700

    Complementary DNA

--- complementary-dna.js
+++ complementary-dna.js
@@ -2,11 +2,4 @@
 // Day 1
 // https://www.codewars.com/kata/554e4a2f232cdd87d9000038
 
-// const DNAStrand = (dna) =>
-//   dna
-//     .split("")
-//     .map((x) => (x === "A" ? "T" : x === "T" ? "A" : x === "C" ? "G" : "C"))
-//     .join("");
-
 const DNAStrand = (dna) => dna.replace(/./g, (m) => "CGAT"["GCTA".indexOf(m)]);
-console.log(DNAStrand("ATTGC"));
