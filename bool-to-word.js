ac093b54166ae2a0404e7b4a690d5a93b028ebf4
Blurryface <2223594.elgin@smkn-2sbg.sch.id>
2024-05-20 10:29:52 +0700

    Sum Of Positive

---
+++ bool-to-word.js
@@ -0,0 +1,5 @@
+// Bool To Word
+// Day 5
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+const boolToWord = (bool) => (bool === true ? "Yes" : "No");
---
+++ sum-of-positive.js
@@ -0,0 +1,7 @@
+// Bool To Word
+// Day 5
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+const positiveSum = (...arr) =>
+  arr.reduce((acc, curr) => (curr > 0 ? acc + curr : acc), 0);
+console.log(positiveSum(1, 2, 3, 4, 5));
