91504e036920a43c1b6c0ef354df623b358aec74
Blurryface <2223594.elgin@smkn-2sbg.sch.id>
2024-05-20 14:19:04 +0700

    Opposite Number

---
+++ opposite-number.js
@@ -0,0 +1,6 @@
+// Summation
+// Day 10
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+const opposite = (n) => (n === Math.sign(0) ? (n = n) : (n = -n));
+// const opposite = n => -n;
